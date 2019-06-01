bd8143ecd3e4c1d593b0f2e6aa8354fef8b2c89a
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-06-01 16:16:30 +0530

    Setup any state animations for death animations , trigger death animations - stop attacking when enemy died BUG: player starts hitting enemy facing in any direction even in the direction opp to enemy looks annoying and wierd .done till 39

--- Assets/Scripts/Combat/CombatTarget.cs
+++ Assets/Scripts/Combat/CombatTarget.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace RPG.Combat
 {
+    [RequireComponent(typeof(Health))]  //will automatically place one if not available .
     public class CombatTarget : MonoBehaviour
     {
 
